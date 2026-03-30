bb987c4998f8d882797812ed568b9733b5e2bd01
Gladina Sherby <gladinasherby@gmail.com>
2026-03-30 22:17:56 +0530

    changed vit again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "infinite-xo", // Using './' ensures it works on any GitHub URL
+  base: "/infinite-xo/", // Using './' ensures it works on any GitHub URL
 });
