d3f951f5a943ef820ffed7e5a5899594094e15f2
Gladina Sherby <gladinasherby@gmail.com>
2026-03-30 11:22:13 +0530

    Fix: relative paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/infinite-xo/", // Replace 'your-repo-name' with your actual GitHub repo name
+  base: "/infinite-xo/",
 });
